6590468902c14be4612fe3091a45f83d984df9f9
shreyaaa-patil <v.shreyaa.patil@gmail.com>
2025-11-08 21:15:44 +0530

    changes made in asking whether user wants to continue

--- graph.c
+++ graph.c
@@ -1,114 +1,126 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "flood.h"
 
-void initGraph(Graph *g) {
-    g->numAreas = 0;
-    for (int i = 0; i < MAX_AREAS; i++) {
-        for (int j = 0; j < MAX_AREAS; j++)
-            g->adj[i][j] = 0;
-        g->delivered[i] = 0;
-    }
+// helper function to safely read line input
+void readLine(char *str, int size) {
+    fgets(str, size, stdin);
+    str[strcspn(str, "\n")] = '\0'; // remove newline
 }
 
-int findAreaIndex(Graph *g, char name[]) {
-    for (int i = 0; i < g->numAreas; i++) {
-        if (strcmp(g->areaNames[i], name) == 0)
-            return i;
-    }
-    return -1;
+// helper to get integer input safely
+int getIntInput(const char *prompt) {
+    char buf[20];
+    int val;
+    printf("%s", prompt);
+    fgets(buf, sizeof(buf), stdin);
+    val = atoi(buf);
+    return val;
 }
 
-void addArea(Graph *g, char name[]) {
-    if (g->numAreas >= MAX_AREAS) {
-        printf("Cannot add more areas.\n");
-        return;
-    }
-
-    size_t cap = sizeof g->areaNames[g->numAreas]; // capacity for one name
-    char tmp[sizeof g->areaNames[0]];
-    // Copy safely and ensure null-termination
-    strncpy(tmp, name, cap - 1);
-    tmp[cap - 1] = '\0';
-
-    // Trim trailing newline/carriage return if caller used fgets
-    size_t len = strlen(tmp);
-    while (len > 0 && (tmp[len - 1] == '\n' || tmp[len - 1] == '\r')) {
-        tmp[--len] = '\0';
-    }
-
-    if (len == 0) {
-        printf("Name cannot be empty.\n");
-        return;
-    }
-
-    if (findAreaIndex(g, tmp) != -1) {
-        printf("Area already exists.\n");
-        return;
-    }
-
-    // Copy into graph storage
-    strncpy(g->areaNames[g->numAreas], tmp, cap - 1);
-    g->areaNames[g->numAreas][cap - 1] = '\0';
-
-    g->numAreas++;
-    printf("Area '%s' added successfully.\n", tmp);
+// ask user if they want to continue an operation
+int askContinue(const char *msg) {
+    char choice[5];
+    printf("%s (y/n): ", msg);
+    readLine(choice, sizeof(choice));
+    return (choice[0] == 'y' || choice[0] == 'Y');
 }
 
-void deleteArea(Graph *g, char name[]) {
-    int index = findAreaIndex(g, name);
-    if (index == -1) {
-        printf("Area not found.\n");
-        return;
-    }
+int main() {
+    Queue q;
+    Graph g;
+    initQueue(&q);
+    initGraph(&g);
 
-    // Shift rows and columns
-    for (int i = index; i < g->numAreas - 1; i++) {
-        strcpy(g->areaNames[i], g->areaNames[i + 1]);
-        g->delivered[i] = g->delivered[i + 1];
-        for (int j = 0; j < g->numAreas; j++)
-            g->adj[i][j] = g->adj[i + 1][j];
-    }
-    for (int j = index; j < g->numAreas - 1; j++) {
-        for (int i = 0; i < g->numAreas; i++)
-            g->adj[i][j] = g->adj[i][j + 1];
-    }
+    int choice, num;
+    char area[50], src[50], dest[50];
 
-    g->numAreas--;
-    printf("Area '%s' deleted successfully.\n", name);
-}
+    while (1) {
+        printf("\n==== FLOOD RELIEF MANAGEMENT ====\n");
+        printf("1. Add stock to inventory\n");
+        printf("2. Remove stock for transport\n");
+        printf("3. Add area\n");
+        printf("4. Delete area\n");
+        printf("5. Add connectivity\n");
+        printf("6. Display graph\n");
+        printf("7. Mark area as delivered\n");
+        printf("8. Display stock\n");
+        printf("0. Exit\n");
 
-void addConnection(Graph *g, char src[], char dest[]) {
-    int s = findAreaIndex(g, src);
-    int d = findAreaIndex(g, dest);
+        choice = getIntInput("Enter choice: ");
 
-    if (s == -1 || d == -1) {
-        printf("Both areas must exist before connecting.\n");
-        return;
-    }
-    g->adj[s][d] = g->adj[d][s] = 1;
-    printf("Connection added between '%s' and '%s'.\n", src, dest);
-}
+        switch (choice) {
+            case 1:
+                do {
+                    num = getIntInput("Enter number of packages to add: ");
+                    for (int i = 0; i < num; i++) {
+                        if (isFull(&q)) {
+                            printf("Inventory full! %d packages added so far.\n", i);
+                            break;
+                        }
+                        enqueue(&q, 1);
+                    }
+                    displayQueue(&q);
+                } while (askContinue("Add more packages?"));
+                break;
 
-void displayGraph(Graph *g) {
-    printf("\n--- Areas and Connections ---\n");
-    for (int i = 0; i < g->numAreas; i++) {
-        printf("%s [%s] --> ", g->areaNames[i],
-               g->delivered[i] ? "Delivered" : "Pending");
-        for (int j = 0; j < g->numAreas; j++) {
-            if (g->adj[i][j])
-                printf("%s ", g->areaNames[j]);
+            case 2:
+                do {
+                    num = getIntInput("Enter number of packages to transport: ");
+                    dequeue(&q, num);
+                    displayQueue(&q);
+                } while (askContinue("Remove more packages?"));
+                break;
+
+            case 3:
+                do {
+                    printf("Enter area name: ");
+                    readLine(area, sizeof(area));
+                    addArea(&g, area);
+                } while (askContinue("Add another area?"));
+                break;
+
+            case 4:
+                do {
+                    printf("Enter area name to delete: ");
+                    readLine(area, sizeof(area));
+                    deleteArea(&g, area);
+                } while (askContinue("Delete another area?"));
+                break;
+
+            case 5:
+                do {
+                    printf("Enter source area: ");
+                    readLine(src, sizeof(src));
+                    printf("Enter destination area: ");
+                    readLine(dest, sizeof(dest));
+                    addConnection(&g, src, dest);
+                } while (askContinue("Add another connection?"));
+                break;
+
+            case 6:
+                displayGraph(&g);
+                break;
+
+            case 7:
+                do {
+                    printf("Enter area name to mark delivered: ");
+                    readLine(area, sizeof(area));
+                    markDelivered(&g, area);
+                } while (askContinue("Mark another area as delivered?"));
+                break;
+
+            case 8:
+                displayQueue(&q);
+                break;
+
+            case 0:
+                printf("Exiting system. Stay safe!\n");
+                return 0;
+
+            default:
+                printf("Invalid choice.\n");
         }
-        printf("\n");
     }
-}
-
-void markDelivered(Graph *g, char name[]) {
-    int index = findAreaIndex(g, name);
-    if (index == -1) {
-        printf("Area not found.\n");
-        return;
-    }
-    g->delivered[index] = 1;
-    printf("Marked '%s' as delivered.\n", name);
 }
